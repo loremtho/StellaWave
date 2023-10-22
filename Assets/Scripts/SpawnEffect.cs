7cc0a6510736192e6a0096b58a2632f4772e3b3f
loremtho <133847848+loremtho@users.noreply.github.com>
2023-10-23 02:37:06 +0900

    샤라락 완료

--- Assets/Scripts/SpawnEffect.cs
+++ Assets/Scripts/SpawnEffect.cs
@@ -4,16 +4,41 @@ using UnityEngine;
 
 public class SpawnEffect : MonoBehaviour
 {
-    [SerializeField] private Renderer renderer;
-    [SerializeField] private Material materialOrg;
-    [SerializeField] private Material materialPhase;
+    [Header("렌더러")]
+    [SerializeField] private Renderer[] Headrenderers;
+    [SerializeField] private Renderer[] Bodyrenderers;
+
+    [Header("메테리얼")]
+
+    [SerializeField] private Material[] HeadOrgmat; 
+    [SerializeField] private Material[] BodyOrgmat; 
     [SerializeField] private Material materialDissolve;
+
+    [Header("오브젝트 껏다키기")]
+    [SerializeField] private GameObject Eyes; 
     [SerializeField] private  float fadeTime = 2f;
     // Start is called before the first frame update
     void Start()
     {
-        renderer.material = materialDissolve;
+        Bodyrenderers[0].material = materialDissolve;
         Dofade(0,1,fadeTime);
+        Bodyrenderers[1].material = materialDissolve;
+        Dofade(0,1,fadeTime);
+        Bodyrenderers[2].material = materialDissolve;
+        Dofade(0,1,fadeTime);
+
+
+        Headrenderers[0].material = materialDissolve;
+        Dofade(0,1,fadeTime);
+        Headrenderers[1].material = materialDissolve;
+        Dofade(0,1,fadeTime);
+        Headrenderers[2].material = materialDissolve;
+        Dofade(0,1,fadeTime);
+        Headrenderers[3].material = materialDissolve;
+        Dofade(0,1,fadeTime);
+
+        this.Eyes.SetActive(false);
+
     }
 
 
@@ -23,12 +48,21 @@ public class SpawnEffect : MonoBehaviour
                     , "onupdate", "TweenOnUpdate", "oncomplete", "TweenOnComplete", "essetype", iTween.EaseType.easeInCubic));
     }
 
-    void TweenOnUpdate(float value)
+    /*void TweenOnUpdate(float value)
     {
-        renderer.material.SetFloat("_Split_Value", value);
-    }
+        GetComponent<Renderer>().material.SetFloat("_Split_Value", value);
+    }*/
     void TweenOnComplete()
     {
-        renderer.material = materialOrg;
+        Bodyrenderers[0].material = BodyOrgmat[0];
+        Bodyrenderers[1].material = BodyOrgmat[1];
+        Bodyrenderers[2].material = BodyOrgmat[2];
+
+        Headrenderers[0].material = HeadOrgmat[0];
+        Headrenderers[1].material = HeadOrgmat[1];
+        Headrenderers[2].material = HeadOrgmat[2];
+        Headrenderers[3].material = HeadOrgmat[3];
+        
+        this.Eyes.SetActive(true);
     }
 }
