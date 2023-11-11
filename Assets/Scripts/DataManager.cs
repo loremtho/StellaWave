e778c5b74fd9ef158083505a76e880b258f013b9
loremtho <133847848+loremtho@users.noreply.github.com>
2023-11-11 21:19:21 +0900

    제이슨 구현

--- Assets/Scripts/DataManager.cs
+++ Assets/Scripts/DataManager.cs
@@ -1,16 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class DataManager : MonoBehaviour
+public class DataManager : MonoBehaviour //변경된 데이터 관리 스크립트
 {
-    // Start is called before the first frame update
+ 
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
