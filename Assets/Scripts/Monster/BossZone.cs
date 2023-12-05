c9d9c0938f32a4a8d398ed51e97d478514663c10
bgbg <bg63645539@gmail.com>
2023-12-05 23:31:56 +0900

    이미지, 스토리 작업

--- Assets/Scripts/Monster/BossZone.cs
+++ Assets/Scripts/Monster/BossZone.cs
@@ -5,9 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class BossZone : MonoBehaviour
 {
+    [SerializeField]
+    private string bgm2;
+
     private void OnTriggerEnter(Collider other) {
         if(other.gameObject.tag == "Player")
         {
+            SoundManager.instance.PlayBGM(bgm2); 
             SceneManager.LoadScene("BossStage1");
         }
     }
