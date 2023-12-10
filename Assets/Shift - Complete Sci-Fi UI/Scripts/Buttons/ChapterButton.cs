ef9effd55af8c27e0d4f8ae431afd20749a33831
bgbg <bg63645539@gmail.com>
2023-12-10 22:45:24 +0900

    메인 로비 오류 수정

--- Assets/Shift - Complete Sci-Fi UI/Scripts/Buttons/ChapterButton.cs
+++ Assets/Shift - Complete Sci-Fi UI/Scripts/Buttons/ChapterButton.cs
@@ -47,29 +47,29 @@ namespace Michsky.UI.Shift
 
             if (enableStatus == true)
             {
-                statusNone = gameObject.transform.Find("Content/Texts/Status/None").GetComponent<Transform>();
-                statusLocked = gameObject.transform.Find("Content/Texts/Status/Locked").GetComponent<Transform>();
-                statusCompleted = gameObject.transform.Find("Content/Texts/Status/Completed").GetComponent<Transform>();
+                //statusNone = gameObject.transform.Find("Content/Texts/Status/None").GetComponent<Transform>();
+               // statusLocked = gameObject.transform.Find("Content/Texts/Status/Locked").GetComponent<Transform>();
+               // statusCompleted = gameObject.transform.Find("Content/Texts/Status/Completed").GetComponent<Transform>();
 
                 if (statusItem == StatusItem.None)
                 {
-                    statusNone.gameObject.SetActive(true);
-                    statusLocked.gameObject.SetActive(false);
-                    statusCompleted.gameObject.SetActive(false);
+                    //statusNone.gameObject.SetActive(true);
+                    //statusLocked.gameObject.SetActive(false);
+                    //statusCompleted.gameObject.SetActive(false);
                 }
 
                 else if (statusItem == StatusItem.Locked)
                 {
-                    statusNone.gameObject.SetActive(false);
-                    statusLocked.gameObject.SetActive(true);
-                    statusCompleted.gameObject.SetActive(false);
+                    //statusNone.gameObject.SetActive(false);
+                    //statusLocked.gameObject.SetActive(true);
+                    //statusCompleted.gameObject.SetActive(false);
                 }
 
                 else if (statusItem == StatusItem.Completed)
                 {
-                    statusNone.gameObject.SetActive(false);
-                    statusLocked.gameObject.SetActive(false);
-                    statusCompleted.gameObject.SetActive(true);
+                    //statusNone.gameObject.SetActive(false);
+                    //statusLocked.gameObject.SetActive(false);
+                    //statusCompleted.gameObject.SetActive(true);
                 }
             }
         }
